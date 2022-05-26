99b3ce44f46bc0615b58085cb3280658d34ac1b8
Ellyson <xxLAxxjeje09@gmail.com>
2022-05-26 21:37:23 +0800

    Exercise 3.8

---
+++ 3.8 Coding Exercises.py
@@ -0,0 +1,22 @@
+# step1
+f = open("pythonessay.txt", "r")
+
+# step2
+print(f.read())
+
+# step3
+print(f.readline())
+
+# step4
+f = open("pythonessay.txt", "r")
+for x in f:
+    print(x)
+
+# step 5
+import os
+
+if os.path.exists("pythonessay.txt"):
+    os.remove("pythonessay.txt")
+else:
+    print("The file does not exist")
+

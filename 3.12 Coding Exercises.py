2b1584c3f710387c778576230adea4944af00f80
Ellyson <xxLAxxjeje09@gmail.com>
2022-05-26 21:40:52 +0800

    Exercise 3.12.1

--- 3.12 Coding Exercises.py
+++ 3.12 Coding Exercises.py
@@ -1,21 +1,3 @@
-
-#step1
-f = open("pythonessay.txt", "r")
-
-#step2
-print(f.read())
-
-#step3
-print(f.readline())
-
-#step4
-f = open("pythonessay.txt", "r")
-for x in f:
-    print(x)
-
-#step 5
-import os
-if os.path.exists("pythonessay.txt"):
-    os.remove("pythonessay.txt")
-else:
-    print("The file does not exist")
+x = 500
+if x > 100:
+    raise Exception("This code will result in an error if x is bigger than 100.")

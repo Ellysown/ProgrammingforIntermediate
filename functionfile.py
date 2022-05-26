e677db6a9da7bc024cdd27fcdc0cd932152e6b53
Ellyson <xxLAxxjeje09@gmail.com>
2022-05-27 01:43:00 +0800

    Exercise 4.4

---
+++ functionfile.py
@@ -0,0 +1,14 @@
+def say_hi():
+    print("hi")
+
+def say_burger_elly(name):
+    print("Hi, " + name + "!")
+
+def favorite_anime(anime):
+    print(anime + " is the most popular anime right now.")
+
+def get_Sum(x,y):
+    print(x + y)
+
+def get_Product(x,y,z):
+    print(x * y * z)
---
+++ main.py
@@ -0,0 +1,5 @@
+from functionfile import favorite_anime
+from functionfile import get_Product
+
+favorite_anime("Ousama Ranking")
+get_Product(5, 5 ,5)
